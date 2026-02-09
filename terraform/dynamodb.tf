b848cdb526b3fb2cdf75d36d222b4ef6758facab
niraj <nirajaws@gmail.com>
2026-02-09 20:20:06 +0530

    created terraform files dynamedb

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamodb_table" "file_logs" {
+  name         = "FileLogs"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "fileID"
+
+  attribute {
+    name = "fileID"
+    type = "S"
+  }
+}
