3012d136666b5b130da05b82a726b2fe104c3c6e
niraj <nirajaws@gmail.com>
2026-02-13 19:41:15 +0530

    changed the error in iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -47,5 +47,5 @@ resource "aws_iam_policy" "lambda_policy" {
 
 resource "aws_iam_role_policy_attachment" "attach" {
     role =  aws_iam_role.lambda_role.name
-    policy_arn = aws_iam_policy.lambda_policy.name 
+    policy_arn = aws_iam_policy.lambda_policy.arn 
 }
